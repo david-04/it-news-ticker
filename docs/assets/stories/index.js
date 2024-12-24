cc9e6f02fc994df4831f4ed0cd6c4c842645576f
David Hofmann <65802756+david-04@users.noreply.github.com>
2024-12-25 10:23:55 +1100

    Initial commit

---
+++ docs/assets/stories/index.js
@@ -0,0 +1,16 @@
+const ENCRYPTION_PARAMETERS = {
+    "encryptionAlgorithm": "AES-GCM",
+    "hash": "SHA-512",
+    "iterations": 250000,
+    "iv": "ec7FMiLFca4oMc916YUSOw==",
+    "keyDerivationAlgorithm": "PBKDF2",
+    "length": 256,
+    "salt": "KHHNxR7Am40wu9oVBMw4Yw=="
+};
+
+const RELEASE_DATES = [
+    "2025-01-07",
+    "2025-01-14",
+    "2025-01-21",
+    "2025-01-29"
+];
