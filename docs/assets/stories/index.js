5b28d9e74349a489de68ad40034fd2465a09d088
David Hofmann <65802756+david-04@users.noreply.github.com>
2024-12-25 10:23:55 +1100

    Initial commit

---
+++ docs/assets/stories/index.js
@@ -0,0 +1,13 @@
+const ENCRYPTION_PARAMETERS = {
+    "encryptionAlgorithm": "AES-GCM",
+    "hash": "SHA-512",
+    "iterations": 250000,
+    "iv": "ec7FMiLFca4oMc916YUSOw==",
+    "keyDerivationAlgorithm": "PBKDF2",
+    "length": 256,
+    "salt": "KHHNxR7Am40wu9oVBMw4Yw=="
+};
+
+const RELEASE_DATES = [
+    "2025-01-07"
+];
---
+++ src/components/Edition.tsx
@@ -0,0 +1,62 @@
+import "./Edition.css";
+
+import { Component, h } from "preact";
+import { FailedTo, Types } from "../util/types.js";
+import { Loading } from "./Loading.js";
+import { Story } from "./Story.js";
+
+//----------------------------------------------------------------------------------------------------------------------
+// Properties and state
+//----------------------------------------------------------------------------------------------------------------------
+
+export interface EditionProps {
+    readonly date: string;
+    readonly edition: Types.Edition;
+}
+
+type EditionState = {};
+
+//----------------------------------------------------------------------------------------------------------------------
+// Issues
+//----------------------------------------------------------------------------------------------------------------------
+
+export class Edition extends Component<EditionProps, EditionState> {
+    //
+    //------------------------------------------------------------------------------------------------------------------
+    // Render the component
+    //------------------------------------------------------------------------------------------------------------------
+
+    render() {
+        return (
+            <div class="Edition">
+                <ul class="list-group">
+                    <li class="list-group-item  bg-primary text-light">{this.props.edition.date}</li>
+                    {this.renderStories()}
+                </ul>
+            </div>
+        );
+    }
+
+    private renderStories() {
+        if (this.props.edition.stories === FailedTo.LOAD) {
+            return this.renderError("⚠️ Failed to load the stories. Please try reload the page.");
+        } else if (this.props.edition.stories === FailedTo.DECRYPT) {
+            return this.renderError("⚠️ Failed to decrypt the stories. Please try reload the page.");
+        } else if (this.props.edition.stories === undefined) {
+            return (
+                <li class="list-group-item">
+                    <Loading global={false} />
+                </li>
+            );
+        } else {
+            this.props.edition.stories satisfies Types.Stories;
+            return this.props.edition.stories.map((story, index) => (
+                <Story story={story} key={`story-${this.props.date}-${index}}`} />
+            ));
+        }
+    }
+
+    private renderError(message: string) {
+        return <li class="list-group-item">{message}</li>;
+    }
+}
